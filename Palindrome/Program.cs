3927872b75de4a08bc87b528dffdd6e3f0fd2091
Behnaz <behnaz.shojaei88@gmail.com>
2025-05-09 12:41:20 +0800

    initial commit

---
+++ Palindrome/Program.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+// remove spaces, remove special characters, everychar to lower then compare each letter from the beginning and last return true or otherwise
+
+theStr = Regex.Replace(theStr, "[^a-zA-Z0-9]", "").ToLower();
